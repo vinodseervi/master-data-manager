43d3e76ab87f411e9d76d83eff8534ca0b68fd8a
Vinod Seervi <65346730+vinodseervi@users.noreply.github.com>
2022-12-19 22:11:33 +0530

    [HealthCheck] : Added Health Check APIs

---
+++ src/main/java/com/vi/models/Response.java
@@ -0,0 +1,16 @@
+package com.vi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Response {
+    private String status;
+    private Object payload;
+
+}
