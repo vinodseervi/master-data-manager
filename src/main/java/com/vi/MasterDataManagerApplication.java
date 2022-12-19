66ebc4b592c34f1b04873be56f70a3baa37bfa5f
Vinod Seervi <65346730+vinodseervi@users.noreply.github.com>
2022-12-19 20:07:32 +0530

    [INIT-SETUP] : inital Setup

---
+++ src/main/java/com/vi/MasterDataManagerApplication.java
@@ -0,0 +1,13 @@
+package com.vi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterDataManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterDataManagerApplication.class, args);
+	}
+
+}
