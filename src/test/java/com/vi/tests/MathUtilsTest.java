e06a9c3facd958517b0a33c7bd4bf7e5ee903359
Vinod Seervi <65346730+vinodseervi@users.noreply.github.com>
2022-12-22 09:32:26 +0530

    [First-Test] : First test case

--- src/test/java/com/vi/tests/MathUtilsTest.java
+++ src/test/java/com/vi/tests/MathUtilsTest.java
@@ -6,18 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
     @Test
-    void addTest(){
+    void addTest() {
         MathUtils mathUtils = new MathUtils();
         int expexted = 2;
-        int actual = mathUtils.add(1,1);
-        assertEquals(expexted,actual);
+        int actual = mathUtils.add(1, 1);
+        assertEquals(expexted, actual);
         System.out.println("This test run");
 
     }
+
     @Test
-    void  testComputeCircleRadiusTest(){
+    void testComputeCircleRadiusTest() {
         MathUtils mathUtils = new MathUtils();
-        assertEquals(314.1592653589793,mathUtils.computeCircleArea(10), "should return right area value");
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "should return right area value");
     }
 
 
