7c5a5abf71222378c214dd33d78821be3282059c
Vinod Seervi <65346730+vinodseervi@users.noreply.github.com>
2022-12-23 01:03:15 +0530

    [JUnit5] : Basic Junit 5 learning

--- src/main/java/com/vi/tests/MathUtils.java
+++ src/main/java/com/vi/tests/MathUtils.java
@@ -4,6 +4,14 @@ public class MathUtils {
     public int add(int a, int b){
         return a+b;
     }
+    public  int multiply(int a,int b){
+        return a*b;
+    }
+
+
+    public int  divide(int a, int b){
+        return a/b;
+    }
     public double computeCircleArea(double radius){
         return Math.PI * radius * radius;
     }
